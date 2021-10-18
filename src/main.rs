061d95dfd8ee110b597abd7e4e38beb0ff402f27
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-10-18 20:52:58 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use github_rs::client::{Executor, Github};
+use serde_json::Value;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let api_token = &args[1];
+    let client = Github::new(api_token).unwrap();
+    let me = client.get().user().execute::<Value>();
+    match me {
+        Ok((headers, status, json)) => {
+            println!("{:#?}", headers);
+            println!("{}", status);
+            if let Some(json) = json {
+                println!("{}", json);
+            }
+        }
+        Err(e) => println!("{}", e),
+    }
 }
