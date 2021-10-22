b6619f6701a72f3586355b15b984dc02e85a90a0
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-10-22 21:05:03 +0900

    edit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use github_rs::client::{Executor, Github};
-use serde_json::Value;
 use std::env;
+
+use github_rs::client::{Executor, Github};
+use github_rs::errors::Error;
+use github_rs::{HeaderMap, StatusCode};
+use serde_json::Value;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let api_token = &args[1];
+    let api_token: &String = &args[1];
     let client = Github::new(api_token).unwrap();
-    let me = client.get().user().execute::<Value>();
-    match me {
+    let pulls = client
+        .get()
+        .repos()
+        .owner("chatwork")
+        .repo("sagrada-server")
+        .pulls()
+        .execute::<Value>();
+    results(pulls);
+}
+
+fn results(pulls: Result<(HeaderMap, StatusCode, Option<Value>), Error>) {
+    match pulls {
         Ok((headers, status, json)) => {
             println!("{:#?}", headers);
             println!("{}", status);
